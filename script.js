106f9fdbb540a7e247abe50c879616d351ac03ff
hamesomer11 <hamzaomer45@gmail.com>
2024-02-18 21:26:04 +0000

    commit #5

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// 1st - get file extension
-// 2nd - convert file to other files with valid extensions
+//    1st - get file extension
+//    2nd - convert file to other files with valid extensions
 
-function validateFileType(fileName) {
-    var fileName = document.getElementById("file-name").value;
-    let fileExtension = fileName.split('.').pop().toLowerCase();
+// function validateFileType() {
+//     //var fileName = document.getElementById("file-name").value;
+//     //let fileExtension = fileName.split('.').pop().toLowerCase();
 
-}
+    
+// }
 
+
+document.getElementById('file').addEventListener('change', function(e) {
+    if (e.target.files[0]) {
+      document.body.append(e.target.files[0].name);
+    }
+});
