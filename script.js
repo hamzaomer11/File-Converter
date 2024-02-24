ec8267897c4c5568913801c55812b7a1af311058
hamesomer11 <hamzaomer45@gmail.com>
2024-02-24 19:43:07 +0000

    Commit #11

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+
 document.getElementById('file').addEventListener('change', function(e) {
     for(var i = 0; i < e.target.files.length; i++) {
       if (e.target.files[i]) {
-        let file = e.target.files[i].name;
+        let file = "File: " + e.target.files[i].name;
         let newFile = file.replace(".pdf", ".docx")
         document.getElementById("file-name").innerHTML = newFile;
       }
